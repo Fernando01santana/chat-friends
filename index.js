fff0e6f47fc51e4fdfbee21801c377821054ac53
Fernando01santana <cool.fernando23@gmail.com>
2021-02-04 00:44:50 -0300

    add:header

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ app.get('/', (req, res) => {
 app.get('/chat', (req, res) => {
     res.render('chat');
 })
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 http.listen(process.env.PORT || 3000, () => {
     console.log("SERVER ON")
 })
